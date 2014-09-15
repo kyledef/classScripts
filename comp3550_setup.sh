545107707e75a72362c630b81ad00517ddd9e7ed
Kyle E DeFreitas <kyle.e.defreitas@gmail.com>
2014-09-15 15:02:54 -0400

    correct break loop error and added curl to installation

--- comp3550_setup.sh
+++ comp3550_setup.sh
@@ -8,7 +8,7 @@ sudo add-apt-repository -y ppa:paolorotolo/android-studio
 sudo apt-get update
 
 
-sudo apt-get install -y git oracle-java8-installer sublime-text-installer nodejs nodejs-legacy npm apache2 php5 php5-mysql mysql-server-5.6 mysql-client-5.6 phpmyadmin php5-mcrypt
+sudo apt-get install -y git oracle-java8-installer sublime-text-installer nodejs nodejs-legacy npm apache2 php5 php5-mysql mysql-server-5.6 mysql-client-5.6 phpmyadmin php5-mcrypt curl
 
 
 # Install dependencies for PHP development
@@ -34,12 +34,14 @@ sudo npm install -g grunt-cli bower yo
 
 mkdir ~/dev
 
-sudo apt-get dist-upgrade -y
-
-read -p "Do you wish to restart now to complete installation? Y/n" yn
+read -p "Would you like to upgrade all the software in the system. Note while this is advisable it will take a considerable amount of time. Y/n " yn
 case $yn in
-    [Yy]* ) sudo reboot; break;;
-    [Nn]* ) exit;;
-    * ) echo "Please answer [y]yes or [n]no.";;
+    [Yy]* ) sudo apt-get dist-upgrade -y;
 esac
-done
+
+
+
+read -p "Do you wish to restart now to complete installation? Y/n " yn
+case $yn in
+    [Yy]* ) sudo reboot;
+esac
