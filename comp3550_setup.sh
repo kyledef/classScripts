b59f7e27216f5a52da230a0064a9309d8253067d
Kyle DeFreitas <kyle.e.defreitas@gmail.com>
2015-09-02 09:58:57 -0400

    added python pip and virtualenv

--- comp3550_setup.sh
+++ comp3550_setup.sh
@@ -7,7 +7,7 @@ sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 # Remove requirements for user confirmation of java liscense
 sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-sudo apt-get install -y git oracle-java8-installer sublime-text-installer curl
+sudo apt-get install -y git oracle-java8-installer sublime-text-installer curl nautilus-open-terminal
 
 
 read -p "Install Nodejs y/N " yn
@@ -32,10 +32,17 @@ case $yn in
 		gulp -v
 esac
 
+read -p "Install python development tools y/N" yn
+case $yn in
+	[Yy]* ) 
+		sudo apt-get install -y python-pip python-virtualenv python-dev libblas-dev liblapack-dev python-dev libatlas-base-dev gfortran
+esac
+
+
 read -p "Install LAMP Stack y/N" yn
 case $yn in
 	[Yy]* ) 
-		sudo apt-get install -y apache2 php5 php5-mysql mysql-server-5.6 mysql-client-5.6 phpmyadmin php5-mcrypt
+		sudo apt-get install -y apache2 php5 php5-mysql mysql-server-5.6 mysql-client-5.6 phpmyadmin php5-mcrypt apache2-utils
 		echo "Configuring Apache Installation"
 		sudo a2enmod rewrite
 		echo "Change the default.conf file to AllowOverride All"
@@ -49,16 +56,10 @@ case $yn in
 		composer --version
 esac
 
-
-
-
-
 read -p "Would you like to upgrade all the software in the system. Note while this is advisable it will take a considerable amount of time. y/N " yn
 case $yn in
     [Yy]* ) sudo apt-get dist-upgrade -y;
 esac
-
-
 
 read -p "Do you wish to restart now to complete installation? y/N " yn
 case $yn in
